8e324301109df1b50ad75d961c68a03dc89cdbe8
Devan Huapaya <huapayadevan@gmail.com>
2017-08-22 15:19:26 -0500

    :hammer:

--- src/views/blog/Blog.js
+++ src/views/blog/Blog.js
@@ -10,8 +10,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightTheme from '../../config/lightTheme';
 
-import Fuse from 'fuse.js'; // PROVIDES FUZZY SEARCH
-import SkeletonBox from '../../components/SkeletonBox';
+// import Fuse from 'fuse.js'; // PROVIDES FUZZY SEARCH
+// import SkeletonBox from '../../components/SkeletonBox';
 import Posts from '../../components/Posts';
 import Footer from '../../components/Footer';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
@@ -19,14 +19,10 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 class BlogPage extends React.Component{
   constructor(props, context){
     super(props, context);
-    this.state = {
-      activeCategories: [],
-      search: ''
-    };
-    this.setActiveCategory = this.setActiveCategory.bind(this);
-    this.renderCategories = this.renderCategories.bind(this);
+    
+    // this.setActiveCategory = this.setActiveCategory.bind(this);
     this.renderPosts = this.renderPosts.bind(this);
-    this.setSearch = this.setSearch.bind(this);
+    // this.setSearch = this.setSearch.bind(this);
   }
 
   componentWillMount(){
@@ -38,107 +34,31 @@ class BlogPage extends React.Component{
     window.scrollTo(0, 0)
   }
   
-  setActiveCategory(category){
-    if(this.state.activeCategories.includes(category)){
-      this.setState({...this.state,
-        activeCategories: this.state.activeCategories.filter((_category) => _category !== category)
-      })
-    } else {
-      const newCategories = this.state.activeCategories.slice();
-      newCategories.push(category);
-      this.setState({activeCategories:newCategories})
-    }
+  // filterPostsByFuseSearch(posts, search){
+  //   if(posts && search){
+  //     var options = {
+  //       shouldSort: true,
+  //       threshold: 0.7,
+  //       location: 0,
+  //       distance: 100,
+  //       maxPatternLength: 32,
+  //       minMatchCharLength: 3,
+  //       keys: [
+  //         "title.rendered"
+  //       ]
+  //     };
+  //     var fuse = new Fuse(posts, options); // "list" is the item array
+  //     var result = fuse.search(search);
+  //     return result
+  //   }else{
+  //     return posts
+  //   }
+  // }
+
+  renderPosts(posts){
+    return(posts.filter(p => p.categories.includes(211)));
   }
 
-  renderCategories(categories){
-    if(categories){
-      return(
-        <div className="categories">
-          {this.filterCategoriesByCount(categories).map((category) => {
-            return(
-              <span key={category.id}
-                className={this.state.activeCategories.includes(category)? 'category selected': 'category'}
-                onClick={this.setActiveCategory.bind(null, category)}>
-                {category.name}
-              </span>
-              )
-            })
-          }
-        </div>
-      )
-    }else{
-      return(
-        <div className="row">
-          <div className="col-8">
-            <SkeletonBox
-              rows={3}
-              boxHeight={'30px'}
-              />
-          </div>
-        </div>
-      )
-    }
-  }
-
-  filterCategoriesByCount(categories){
-    if(categories){
-      return categories.filter((_category) => _category.count > 0)
-    }
-  }
-
-  filterPostsByActiveCategory(posts, activeCategories){
-    if(posts && activeCategories){
-      // let activeCategoriesIDArray = activeCategories.map(category => {return category.id})
-      // let filteredPosts = posts.map(post => {
-      //   if(post._embedded && post._embedded['wp:term']){
-      //     let postCategories = post._embedded['wp:term'][0];
-      //     return postCategories.filter(category => {return category.id === activeCategories[0]})
-      //   }
-      // })
-      // debugger
-      // RETURN POSTS WHERE THE POSTS CATEGORIES MATCH ALL THE ACTIVE CATEGORIES
-
-      return posts
-    }else{
-      return posts
-    }
-  }
-
-  filterPostsByFuseSearch(posts, search){
-    if(posts && search){
-      var options = {
-        shouldSort: true,
-        threshold: 0.7,
-        location: 0,
-        distance: 100,
-        maxPatternLength: 32,
-        minMatchCharLength: 3,
-        keys: [
-          "title.rendered"
-        ]
-      };
-      var fuse = new Fuse(posts, options); // "list" is the item array
-      var result = fuse.search(search);
-      return result
-    }else{
-      return posts
-    }
-  }
-
-  renderPosts(posts, search, activeCategories){
-    return(
-      this.filterPostsByActiveCategory(
-        this.filterPostsByFuseSearch(
-          posts.filter(p => p.categories.includes(211)),
-          search),
-        activeCategories
-      )
-    )
-  }
-
-  setSearch(e){
-    this.setState({search: e.target.value})
-  }
   render(){
     return(
       <MuiThemeProvider muiTheme={getMuiTheme(lightTheme)} >
@@ -163,10 +83,7 @@ class BlogPage extends React.Component{
             <Row className=''>
               <Col sm={12} md={8}>
                   <Posts posts={
-                    this.renderPosts(
-                      this.props.posts,
-                      this.state.search,
-                      this.state.activeCategories)
+                    this.renderPosts(this.props.posts)
                   }/>
               </Col>
               {/* <Col sm={12} md={4}>
