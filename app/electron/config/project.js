9d422146dd878e7304699fdda08031f519b42f3d
Devan Huapaya <huapayadevan@gmail.com>
2018-05-17 17:01:22 -0500

    :nail_care:

--- app/electron/config/project.js
+++ app/electron/config/project.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 export default {
   windowOptions: {
     show: false, // Will be set to true on 'ready'
@@ -5,6 +7,8 @@ export default {
     minHeight: 900,
     frame: true,
     autoHideMenuBar: false, // useful if you want a menu that is hidden only sometimes
+    titleBarStyle: 'hidden',
+    icon: path.join(__dirname, '../../ok-emoji.png'),
     webPreferences: {
       devTools: process.env.NODE_ENV === 'development',
       scrollBounce: true
